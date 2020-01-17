f9dab981c48f37a535e17004d2af51c4b3370254
Zakaria Farhati <zakariafarhati18@gmail.com>
2020-01-17 12:38:38 +0100

    Fix the if statement error
    It's working good now ! :smile_cat: :

--- the script.py
+++ the script.py
@@ -3,7 +3,7 @@ def sayHello(people_name):
   print("hello " + people_name)
   print("How are you doing ? ")
   answer = str(input(">>> "))
-  if answer == "I'm fine" or "Fine" or "Good":
+  if answer == "I'm fine" or answer == "Fine" or answer == "Good":
     print("So you have a nice day!")
   elif answer == "I am not good":
     print("Oh come on make a smile !")
